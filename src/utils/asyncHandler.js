71fcca86b4bc2ef0dc27488e3bda75e697ea829b
webdeveloperNP <nishant24patel@gmail.com>
2024-08-10 16:22:17 +0530

    Add helper Utilities: asynchronous handler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,15 @@
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+    // //try-catch version of above code
+    // try {
+    //   requestHandler(req, res, next);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+};
+
+export { asyncHandler };
