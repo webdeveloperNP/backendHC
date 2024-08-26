34f9143db0cbc17df249f3d6bd48a51174812571
webdeveloperNP <nishant24patel@gmail.com>
2024-08-26 14:59:21 +0530

    inject plugin mongoose-aggregate-paginate-v2 to video schema

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
 const videoSchema = new Schema(
   {
     videofile: {
@@ -25,5 +26,7 @@ const videoSchema = new Schema(
   },
   { timestamps: true }
 );
+
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = new mongoose.Model('Video', videoSchema);
